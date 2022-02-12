b88364d6e477067ef15c43a7e551b08769298a3c
BlackLuny <602814112@qq.com>
2022-02-13 01:21:15 +0800

    save

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-use dis_core::ZkMng;
-fn main() {
-    println!("Hello, world!");
+use std::time::{Duration, Instant};
+
+use dis_core::{DummyTaskBehaviour, Schedular, TaskType};
+#[tokio::main]
+async fn main() {
+    if std::env::args().len() == 1 {
+        let schedular = Schedular::try_new("127.0.0.1:2181", DummyTaskBehaviour {})
+            .await
+            .unwrap();
+        tokio::time::sleep(Duration::from_secs(600)).await;
+    } else {
+        let schedular = Schedular::try_new("127.0.0.1:2181", DummyTaskBehaviour {})
+            .await
+            .unwrap();
+        println!("start");
+        let start = Instant::now();
+        schedular.spawn_task(10, TaskType::Spread, Some(1)).await.unwrap();
+        println!("time used: {}", start.elapsed().as_millis());
+    }
+
 }
